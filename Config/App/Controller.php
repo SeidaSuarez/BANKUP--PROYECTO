f1b303fe56d18a38bda3c83632eabdedc31664c1
SeidaSuarez <115797031+SeidaSuarez@users.noreply.github.com>
2023-12-13 22:14:08 +0100

    Config de esquelo

--- Config/App/Controller.php
+++ Config/App/Controller.php
@@ -1,7 +1,21 @@
 <?php 
     class Controller {
         protected $views, $model;
- 
+
+        public function __construct()
+        {
+            $this->cargarModel();
+        }
+
+        public function cargarModel()
+        {
+            $model = get_class($this)."Model";
+            $ruta = "Models/".$model.".php";
+            if(file_exists($ruta)) {
+                require_once $ruta;
+                $this->model = new $model();
+            }
+        }
 
 }
 
--- index.php
+++ index.php
@@ -19,6 +19,7 @@
             $parametro = trim($parametro, ",");
         }
     }
+    require_once "Config/App/autoload.php";
     $dirControllers = "Controllers/".$controller.".php";
     if (file_exists($dirControllers)) {
         require_once $dirControllers;
