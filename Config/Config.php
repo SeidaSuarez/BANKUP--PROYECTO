147b36fac702117b50a627ad36db6b3302149ad2
SeidaSuarez <115797031+SeidaSuarez@users.noreply.github.com>
2023-12-14 13:24:11 +0100

    Config más cambios

--- Config/Config.php
+++ Config/Config.php
@@ -1,4 +1,8 @@
 <?php
     const base_url = "http://localhost/BANKUP!-PROYECTO/";
-    
+    const host = "localhost";
+    const user = "root";
+    const pass = "";
+    const db = "bankup";
+    const charset = "charset=utf8";
 ?>
